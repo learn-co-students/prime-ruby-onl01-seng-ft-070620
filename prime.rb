11415420cd00422f1aab1ca1b1c3e6e9739d3425
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-07 18:30:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+require 'pry'
+def prime?(num)
+  range = (2..num).to_a
+  range.each do |divi|
+    if num % divi == 0 && num != divi
+      return false
+    end
+  end
+  num > 1
+end
