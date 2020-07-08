62b581a30ff6d9e863fb763b0f203d6906175de6
Joseph cohen <jojocohen57@gmail.com>
2020-07-08 19:00:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  end
+  
+  (2...num).each do |n|
+   if num % n == 0
+     return false
+   end
+ end 
+  
+  return true
+end
+
+  
