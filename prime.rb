243ebcc97be3382c6eb6bcef63665b96f309137f
Jackson Reynolds <jwreynold@gmail.com>
2020-07-08 14:30:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(int)
+  if int <= 3
+    int > 1
+  elsif int == 0 || int == 1 || int.even? || int % 3 == 0
+    false
+  else
+    i = 5
+    while i**2 <= int
+      if int % i == 0 || int % (i+2) == 0
+        return false
+      end #if
+      i += 6
+    end #while
+    true
+  end #if
+
+
+end #prime?
