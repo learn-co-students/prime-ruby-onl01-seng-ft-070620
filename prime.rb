53cf54dd5a9e3be94b2df630e17acf0a610042b8
Amanda Walker <walker.akw@gmail.com>
2020-07-10 03:41:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 def prime?(int)
   return false if int <=1
   return true if int ==2
-  (2...int-1).each {|factor| return false if int % factor == 0}
+  #line 5 is interchangeable with line 6...they do the same thing different ways
+  #(2...int-1).each {|factor| return false if int % factor == 0}
+  return false if ((2...int-1).each.any? {|factor| int % factor == 0})
   return true
 end
