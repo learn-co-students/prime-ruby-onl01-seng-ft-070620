b32ee337da0622c9a25c953433748a3099684ada
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-15 16:37:23 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,26 +1,33 @@
-require 'benchmark'
+# require 'benchmark'
 
-# Add  code here!
+## REWORK
+
+# 1. Build a #prime? method that:
+#    -Takes in an integer argument
+#    -Returns a boolean
+
 def prime?(num)
     return false if num < 2
     (2..num - 1).each do |x|
         if (num % x) == 0
             return false
-        end
-    end
-return true
+        end 
+    end 
+    return true 
 end 
 
-## number has to be 2 or greater
-## an integer is prime if it is only divisible by 1 and itself.
 
-# Testing speed:
-# puts Benchmark.measure {def prime?(num)
-# return false if num < 2
-# (2..num - 1).each do |x|
-#     if (num % x) == 0
-#         return false
-#     end
-# end
-# return true
-# end }
+
+# ## number has to be 2 or greater
+# ## an integer is prime if it is only divisible by 1 and itself.
+
+# # Testing speed:
+# # puts Benchmark.measure {def prime?(num)
+# # return false if num < 2
+# # (2..num - 1).each do |x|
+# #     if (num % x) == 0
+# #         return false
+# #     end
+# # end
+# # return true
+# # end }
